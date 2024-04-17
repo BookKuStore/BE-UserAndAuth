ad9ea1cbde43a5c68246b54eeae1090109bbf619
Fadhil Muhammad <fadhilmuhammad.fd@gmail.com>
2024-04-17 19:10:24 +0700

    initial project

---
+++ src/main/java/id/ac/ui/cs/advprog/bookku/bookku_userauth/BookkuUserauthApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bookku.bookku_userauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookkuUserauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookkuUserauthApplication.class, args);
+	}
+
+}
