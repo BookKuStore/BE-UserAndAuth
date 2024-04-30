9807a5873f0467b68ccc68f75dc68a9df449215f
Fadhil Muhammad <fadhilmuhammad.fd@gmail.com>
2024-04-30 23:31:21 +0700

    Dependencies Configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,50 +23,92 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+
+	implementation("com.google.guava:guava:31.1-jre")
+	implementation("org.hibernate.validator:hibernate-validator:8.0.0.Final")
+	implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+	implementation("org.powermock:powermock-module-junit4:2.0.9")
+	implementation("org.powermock:powermock-api-mockito2:2.0.9")
+
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	testImplementation("org.junit.jupiter:junit-jupiter-api")
 }
 
 tasks.register<Test>("unitTest") {
-    description = "Runs unit tests."
-    group = "verification"
+	description = "Runs unit tests."
+	group = "verification"
 
-    filter {
-        excludeTestsMatching("*FunctionalTest")
-    }
+	filter {
+		excludeTestsMatching("*FunctionalTest")
+	}
 }
 
 tasks.register<Test>("functionalTest") {
-    description = "Runs functional tests."
-    group = "verification"
+	description = "Runs functional tests."
+	group = "verification"
 
-    filter {
-        includeTestsMatching("*FunctionalTest")
-    }
+	filter {
+		includeTestsMatching("*FunctionalTest")
+	}
 }
 
 tasks.withType<Test>().configureEach {
-    useJUnitPlatform()
+	useJUnitPlatform()
 }
 
 tasks.test {
-    filter {
-        excludeTestsMatching("*FunctionalTest")
-    }
+	filter {
+		excludeTestsMatching("*FunctionalTest")
+	}
 
-    finalizedBy(tasks.jacocoTestReport)
+	finalizedBy(tasks.jacocoTestReport)
 }
 
 tasks.jacocoTestReport {
-    dependsOn(tasks.test)
-    reports {
-        xml.required.set(true)
-        csv.required.set(true)
-        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
-    }
+	dependsOn(tasks.test)
 }
+
+
+// tasks.withType<Test> {
+// 	useJUnitPlatform()
+// }
+
+// tasks.test {
+// 	useJUnitPlatform()
+// 	finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+// }
+
+// tasks.jacocoTestReport {
+// 	classDirectories.setFrom(files(classDirectories.files.map {
+// 		fileTree(it) { 
+// 			exclude(
+// 				"**/*Application**", 
+// 				"**/dto/**", 
+// 				"**/exceptions/**", 
+// 				"**/filter/**", 
+// 				"**/config/**", 
+// 				"**/model/**", 
+// 				"**/repository/**", 
+// 				"**/service/Datainitializer") 
+// 			}
+// 	}))
+// 	dependsOn(tasks.test) // tests are required to run before generating the report
+// 	reports {
+// 		xml.required.set(true)
+// 		csv.required.set(false)
+// 		html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+// 	}
+// }
