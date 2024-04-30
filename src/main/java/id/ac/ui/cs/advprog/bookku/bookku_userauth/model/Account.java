75b0a0897d31d1b1200770d922716f2f53004516
Fadhil Muhammad <fadhilmuhammad.fd@gmail.com>
2024-04-30 23:36:48 +0700

    [GREEN] Implement Account model

---
+++ src/main/java/id/ac/ui/cs/advprog/bookku/bookku_userauth/model/Account.java
@@ -0,0 +1,43 @@
+package id.ac.ui.cs.advprog.bookku.bookku_userauth.model;
+
+import org.hibernate.annotations.GenericGenerator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(
+    name = "account",
+    uniqueConstraints = { @UniqueConstraint(columnNames = "username") }
+)
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator="seq")
+    @GenericGenerator(name = "seq", strategy="increment")
+    private Integer id;
+
+    @NonNull
+    private String username;
+
+    @JsonIgnore
+    @NonNull
+    private String password;
+
+    @NonNull
+    private String role;
+
+    @NonNull
+    private String name;
+
+    @NonNull
+    private String email;
+
+    @NonNull
+    private String phone;
+
+}
