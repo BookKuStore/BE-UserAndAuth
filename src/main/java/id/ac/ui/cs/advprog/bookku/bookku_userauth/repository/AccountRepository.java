f6f43fda8d2fc111cb70add09238cd8eadedef30
Fadhil Muhammad <fadhilmuhammad.fd@gmail.com>
2024-05-01 18:31:44 +0700

    [RED] Add tests for Account Service

--- src/main/java/id/ac/ui/cs/advprog/bookku/bookku_userauth/repository/AccountRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/bookku/bookku_userauth/repository/AccountRepository.java
@@ -7,10 +7,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
     Optional<Account> findByUsername(String username);
+
+    List<Account> findAllOptionalByRole(String role);
 
     @Transactional
     void deleteByUsername(String username);
