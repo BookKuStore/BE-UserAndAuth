36db06781a5f4dee2de1c48f293fe5d03161e814
Fadhil Muhammad <fadhilmuhammad.fd@gmail.com>
2024-05-26 02:25:14 +0700

    [REFACTOR] Remove unnecessary imports in Account Controller Test

--- src/test/java/id/ac/ui/cs/advprog/bookku/bookku_userauth/controller/AccountControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/bookku/bookku_userauth/controller/AccountControllerTest.java
@@ -5,17 +5,12 @@ import id.ac.ui.cs.advprog.bookku.bookku_userauth.service.AccountService;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.graphql.AutoConfigureGraphQl;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
