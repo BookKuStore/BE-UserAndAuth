7a88bba691a1a791cbdade0c8845ad9b35e9da2a
Fadhil Muhammad <fadhilmuhammad.fd@gmail.com>
2024-05-26 17:31:30 +0700

    [GREEN] Implement new Account Controllers

--- src/main/java/id/ac/ui/cs/advprog/bookku/bookku_userauth/controller/AccountController.java
+++ src/main/java/id/ac/ui/cs/advprog/bookku/bookku_userauth/controller/AccountController.java
@@ -1,5 +1,8 @@
 package id.ac.ui.cs.advprog.bookku.bookku_userauth.controller;
 
+import id.ac.ui.cs.advprog.bookku.bookku_userauth.dto.ChangePasswordResponse;
+import id.ac.ui.cs.advprog.bookku.bookku_userauth.dto.ChangePasswordRequest;
+import id.ac.ui.cs.advprog.bookku.bookku_userauth.dto.ChangeAttributeResponse;
 import id.ac.ui.cs.advprog.bookku.bookku_userauth.model.Account;
 import id.ac.ui.cs.advprog.bookku.bookku_userauth.service.AccountService;
 
@@ -7,8 +10,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController()
@@ -28,5 +35,16 @@ public class AccountController {
     public ResponseEntity<Account> getAccountByToken(@RequestHeader(name = "Authorization") String accessToken) {
         return ResponseEntity.ok(accountService.getAccountFromToken(accessToken));
     }
+
+    @PostMapping("/change-password")
+    public ResponseEntity<ChangePasswordResponse> postMethodName(@RequestHeader(name = "Authorization") String accessToken, @RequestBody ChangePasswordRequest request) {
+        return ResponseEntity.ok(accountService.changePassword(accessToken, request.getNewPassword()));
+    }
+
+    @PostMapping("/change-attribute")
+    public ResponseEntity<ChangeAttributeResponse> postMethodName(@RequestHeader(name = "Authorization") String accessToken, @RequestBody HashMap<String, String> request) {
+        return ResponseEntity.ok(accountService.changeAttribute(accessToken, request));
+    }
+    
     
 }
