16e1691e78c4979d303c08d8aa452040349ca055
Fadhil Muhammad <fadhilmuhammad.fd@gmail.com>
2024-05-26 13:21:42 +0700

    [CHORES] Modify security filter

--- src/main/java/id/ac/ui/cs/advprog/bookku/bookku_userauth/security/SecurityFilter.java
+++ src/main/java/id/ac/ui/cs/advprog/bookku/bookku_userauth/security/SecurityFilter.java
@@ -20,17 +20,7 @@ public class SecurityFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
 
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-        res.setHeader("Access-Control-Expose-Headers", "Authorization");
-        res.setHeader("Access-Control-Allow-Credentials", "true");
-
-        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
-            res.setStatus(HttpServletResponse.SC_OK);
-        } else {
-            chain.doFilter(request, response);
-        }
+        chain.doFilter(request, response);
     }
 
 }
